efad6868fed0e1f7bba656e9901322f0e20ba740
DimitriiTrater <dima1603040@gmail.com>
2023-06-29 12:57:54 +0200

    First task

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include "include/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+
+#include <iostream>
+
+int main(){
+    // Initialize OpenGL
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    // Initialize Window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "3D_ENGINE", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+
+    // GLAD check
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
+
+    // Win params
+    glViewport(0, 0, 800, 600);
+
+    // Render loop
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    glfwTerminate();
+    return 0;
+
+}
+
+
+
